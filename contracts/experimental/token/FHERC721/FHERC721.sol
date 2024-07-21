4c9a41bd76c156c409b2483f967a8168a950e227
Eshel <eshel@scrtlabs.com>
2024-07-21 18:46:22 +0300

    impl tokenPrivateData

--- contracts/experimental/token/FHERC721/FHERC721.sol
+++ contracts/experimental/token/FHERC721/FHERC721.sol
@@ -5,8 +5,15 @@ import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 import { FHE, euint256, inEuint256 } from "../../../FHE.sol";
 import { Permissioned, Permission } from "../../../access/Permissioned.sol";
-import { IFHERC721 } from "./IFHERC20.sol";
+import { IFHERC721 } from "./IFHERC721.sol";
 
 contract FHERC721 is IFHERC721, Permissioned, ERC721 {
     mapping(uint256 tokenId => euint256) private _privateData;
+
+    function tokenPrivateData(
+        uint256 tokenId,
+        Permission memory auth
+    ) external view onlyPermitted(auth, _owners[tokenId]) returns (string memory) {
+        return FHE.sealoutput(_privateData[tokenId], auth.publicKey);
+    }
 }
