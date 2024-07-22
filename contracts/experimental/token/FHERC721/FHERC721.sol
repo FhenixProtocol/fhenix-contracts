a582de18f01bc91a44265c7ad4d7317629d671eb
Eshel <eshel@scrtlabs.com>
2024-07-22 14:56:48 +0300

    overridding supportsInterface

--- contracts/experimental/token/FHERC721/FHERC721.sol
+++ contracts/experimental/token/FHERC721/FHERC721.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.8.19 <0.9.0;
 
 import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import { Strings } from "@openzeppelin/contracts/utils/Strings.sol";
+import { IERC165 } from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 
 import { FHE, euint256, inEuint256 } from "../../../FHE.sol";
 import { Permissioned, Permission } from "../../../access/Permissioned.sol";
@@ -29,9 +30,9 @@ contract FHERC721 is IFHERC721, Permissioned, ERC721 {
         return bytes(baseURI).length > 0 ? string.concat(baseURI, tokenId.toString()) : "";
     }
 
-    function _mint(address to, uint256 tokenId, inEuint256 privateData) internal {
+    function _mint(address to, uint256 tokenId, inEuint256 calldata privateData) internal {
         super._mint(to, tokenId);
-        privateData[tokenId] = FHE.asEuint256(privateData);
+        _privateData[tokenId] = FHE.asEuint256(privateData);
     }
 
     function tokenPrivateData(
@@ -39,5 +40,12 @@ contract FHERC721 is IFHERC721, Permissioned, ERC721 {
         Permission memory auth
     ) external view onlyPermitted(auth, _ownerOf(tokenId)) returns (string memory) {
         return FHE.sealoutput(_privateData[tokenId], auth.publicKey);
+    }
+
+    /**
+     * @dev See {IERC165-supportsInterface}.
+     */
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, IERC165) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
 }
