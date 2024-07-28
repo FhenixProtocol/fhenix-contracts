c76019b88718531d952cdd4038a0435bc0838590
Eshel <eshel@scrtlabs.com>
2024-07-28 11:07:08 +0300

    adding note on error non-comparison

--- test/helpers/customError.ts
+++ test/helpers/customError.ts
@@ -9,6 +9,9 @@ async function expectRevertCustomError(promise, expectedErrorName, args) {
   await promise.then(
     () => expect.fail("Expected promise to throw but it didn't"),
     ({ message }) => {
+      // NOTE: When a tx revert in fhenix, the message is always the same, meaning we can't check the custom error name.
+      // We could check the tx data, which is returned from the node on eth_calls, but it's stripped out of the error by
+      // one of the web3 libraries.
       if (message !== "Returned error: execution reverted") {
         expect.fail(`Expected 'execution reverted' message`);
       }
