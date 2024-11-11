93621230cdffcfc848983e504d3109f4c384718a
halftone-dev <halftonedev@gmail.com>
2024-11-11 11:52:38 +0000

    typo

--- contracts/FHE.sol
+++ contracts/FHE.sol
@@ -62,7 +62,7 @@ struct SealedBool {
 /// @dev Utility structure providing clients with type context of a sealed output string.
 /// Returned from `FHE.sealoutputTyped` and `sealTyped` within the bindings.
 /// `utype` representing Uints is 0-5. See `FHE.sol` for more.
-/// `utype` map: {uint8: 0} {uint16: 1} {uint32: 1} {uint64: 3} {uint128: 4} {uint256: 5}.
+/// `utype` map: {uint8: 0} {uint16: 1} {uint32: 2} {uint64: 3} {uint128: 4} {uint256: 5}.
 struct SealedUint {
 	string data;
 	uint8 utype;
