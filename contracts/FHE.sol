c0c1e5838df5ba434422c1e5ea9a2e9160a886af
halftone-dev <halftonedev@gmail.com>
2024-11-11 11:52:08 +0000

    Add docstrings to structs

--- contracts/FHE.sol
+++ contracts/FHE.sol
@@ -51,16 +51,26 @@ struct SealedArray {
     bytes[] data;
 }
 
+/// @dev Utility structure providing clients with type context of a sealed output string.
+/// Returned from `FHE.sealoutputTyped` and `sealTyped` within the bindings.
+/// `utype` representing Bool is 13. See `FHE.sol` for more.
 struct SealedBool {
 	string data;
 	uint8 utype;
 }
 
+/// @dev Utility structure providing clients with type context of a sealed output string.
+/// Returned from `FHE.sealoutputTyped` and `sealTyped` within the bindings.
+/// `utype` representing Uints is 0-5. See `FHE.sol` for more.
+/// `utype` map: {uint8: 0} {uint16: 1} {uint32: 1} {uint64: 3} {uint128: 4} {uint256: 5}.
 struct SealedUint {
 	string data;
 	uint8 utype;
 }
 
+/// @dev Utility structure providing clients with type context of a sealed output string.
+/// Returned from `FHE.sealoutputTyped` and `sealTyped` within the bindings.
+/// `utype` representing Address is 12. See `FHE.sol` for more.
 struct SealedAddress {
 	string data;
 	uint8 utype;
