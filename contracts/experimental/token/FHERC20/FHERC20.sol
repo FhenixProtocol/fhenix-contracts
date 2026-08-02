1d9bb40eac60c2408e0bc2c8ebcd64e365535e25
Sertug17 <104278804+Sertug17@users.noreply.github.com>
2026-08-02 22:27:45 +0300

    fix: MockFheOps compile errors, euint256 arithmetic, FHERC20 visibility & events, EIP-712 encoding, test awaits

--- contracts/experimental/token/FHERC20/FHERC20.sol
+++ contracts/experimental/token/FHERC20/FHERC20.sol
@@ -1,138 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.19 <0.9.0;
-
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { FHE, euint128, inEuint128 } from "../../../FHE.sol";
 import { Permissioned, Permission } from "../../../access/Permissioned.sol";
-
 import { IFHERC20 } from "./IFHERC20.sol";
-
 error ErrorInsufficientFunds();
 error ERC20InvalidApprover(address);
 error ERC20InvalidSpender(address);
-
 contract FHERC20 is IFHERC20, ERC20, Permissioned {
-
-    // A mapping from address to an encrypted balance.
     mapping(address => euint128) internal _encBalances;
-    // A mapping from address (owner) to a mapping of address (spender) to an encrypted amount.
     mapping(address => mapping(address => euint128)) internal _allowed;
     euint128 internal totalEncryptedSupply = FHE.asEuint128(0);
-
-    constructor(
-        string memory name,
-        string memory symbol
-    ) ERC20(name, symbol) {}
-
-    function _allowanceEncrypted(address owner, address spender) internal view returns (euint128) {
-        return _allowed[owner][spender];
-    }
-
-    function allowanceEncrypted(
-        address owner,
-        address spender,
-        Permission calldata permission
-    ) public view virtual onlyBetweenPermitted(permission, owner, spender) returns (string memory) {
-        return FHE.sealoutput(_allowanceEncrypted(owner, spender), permission.publicKey);
-    }
-
-    function approveEncrypted(address spender, inEuint128 calldata value) public virtual returns (bool) {
-        _approve(msg.sender, spender, FHE.asEuint128(value));
-        return true;
-    }
-
-    function _approve(address owner, address spender, euint128 value) internal {
-        if (owner == address(0)) {
-            revert ERC20InvalidApprover(address(0));
-        }
-        if (spender == address(0)) {
-            revert ERC20InvalidSpender(address(0));
-        }
-        _allowed[owner][spender] = value;
-    }
-
-    function _spendAllowance(address owner, address spender, euint128 value) internal virtual returns (euint128) {
-        euint128 currentAllowance = _allowanceEncrypted(owner, spender);
-        euint128 spent = FHE.min(currentAllowance, value);
-        _approve(owner, spender, (currentAllowance - spent));
-
-        return spent;
-    }
-
-    function transferFromEncrypted(address from, address to, inEuint128 calldata value) public virtual returns (euint128) {
-        return _transferFromEncrypted(from, to, FHE.asEuint128(value));
-    }
-
-    function _transferFromEncrypted(address from, address to, euint128 value) public virtual returns (euint128) {
-        euint128 val = value;
-        euint128 spent = _spendAllowance(from, msg.sender, val);
-        return _transferImpl(from, to, spent);
-    }
-
-    function wrap(uint32 amount) public {
-        if (balanceOf(msg.sender) < amount) {
-            revert ErrorInsufficientFunds();
-        }
-
-        _burn(msg.sender, amount);
-        euint128 eAmount = FHE.asEuint128(amount);
-        _encBalances[msg.sender] = _encBalances[msg.sender] + eAmount;
-        totalEncryptedSupply = totalEncryptedSupply + eAmount;
-    }
-
-    function unwrap(uint32 amount) public {
-        euint128 encAmount = FHE.asEuint128(amount);
-
-        euint128 amountToUnwrap = FHE.select(_encBalances[msg.sender].gte(encAmount), encAmount, FHE.asEuint128(0));
-
-        _encBalances[msg.sender] = _encBalances[msg.sender] - amountToUnwrap;
-        totalEncryptedSupply = totalEncryptedSupply - amountToUnwrap;
-
-        _mint(msg.sender, FHE.decrypt(amountToUnwrap));
-    }
-
-//    function mint(uint256 amount) public {
-//        _mint(msg.sender, amount);
-//    }
-
-    function _mintEncrypted(address to, inEuint128 memory encryptedAmount) internal {
-        euint128 amount = FHE.asEuint128(encryptedAmount);
-        _encBalances[to] = _encBalances[to] + amount;
-        totalEncryptedSupply = totalEncryptedSupply + amount;
-    }
-
-    function transferEncrypted(address to, inEuint128 calldata encryptedAmount) public returns (euint128) {
-        return _transferEncrypted(to, FHE.asEuint128(encryptedAmount));
-    }
-
-    // Transfers an amount from the message sender address to the `to` address.
-    function _transferEncrypted(address to, euint128 amount) public returns (euint128) {
-        return _transferImpl(msg.sender, to, amount);
-    }
-
-    // Transfers an encrypted amount.
-    function _transferImpl(address from, address to, euint128 amount) internal returns (euint128) {
-        // Make sure the sender has enough tokens.
-        euint128 amountToSend = FHE.select(amount.lte(_encBalances[from]), amount, FHE.asEuint128(0));
-
-        // Add to the balance of `to` and subract from the balance of `from`.
-        _encBalances[to] = _encBalances[to] + amountToSend;
-        _encBalances[from] = _encBalances[from] - amountToSend;
-
-        return amountToSend;
-    }
-
-    function balanceOfEncrypted(
-        address account, Permission memory auth
-    ) virtual public view onlyPermitted(auth, account) returns (string memory) {
-        return _encBalances[account].seal(auth.publicKey);
-    }
-
-    //    // Returns the total supply of tokens, sealed and encrypted for the caller.
-    //    // todo: add a permission check for total supply readers
-    //    function getEncryptedTotalSupply(
-    //        Permission calldata permission
-    //    ) public view onlySender(permission) returns (bytes memory) {
-    //        return totalEncryptedSupply.seal(permission.publicKey);
-    //    }
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+    function _allowanceEncrypted(address owner, address spender) internal view returns (euint128) { return _allowed[owner][spender]; }
+    function allowanceEncrypted(address owner, address spender, Permission calldata permission) public view virtual onlyBetweenPermitted(permission, owner, spender) returns (string memory) { return FHE.sealoutput(_allowanceEncrypted(owner, spender), permission.publicKey); }
+    function approveEncrypted(address spender, inEuint128 calldata value) public virtual returns (bool) { _approve(msg.sender, spender, FHE.asEuint128(value)); return true; }
+    function _approve(address owner, address spender, euint128 value) internal { if (owner == address(0)) revert ERC20InvalidApprover(address(0)); if (spender == address(0)) revert ERC20InvalidSpender(address(0)); _allowed[owner][spender] = value; emit ApprovalEncrypted(owner, spender); }
+    function _spendAllowance(address owner, address spender, euint128 value) internal virtual returns (euint128) { euint128 cur = _allowanceEncrypted(owner, spender); euint128 spent = FHE.min(cur, value); _approve(owner, spender, cur - spent); return spent; }
+    function transferFromEncrypted(address from, address to, inEuint128 calldata value) public virtual returns (euint128) { return _transferImpl(from, to, _spendAllowance(from, msg.sender, FHE.asEuint128(value))); }
+    function wrap(uint32 amount) public { if (balanceOf(msg.sender) < amount) revert ErrorInsufficientFunds(); _burn(msg.sender, amount); euint128 e = FHE.asEuint128(amount); _encBalances[msg.sender] = _encBalances[msg.sender] + e; totalEncryptedSupply = totalEncryptedSupply + e; }
+    function unwrap(uint32 amount) public { euint128 enc = FHE.asEuint128(amount); euint128 toUnwrap = FHE.select(_encBalances[msg.sender].gte(enc), enc, FHE.asEuint128(0)); _encBalances[msg.sender] = _encBalances[msg.sender] - toUnwrap; totalEncryptedSupply = totalEncryptedSupply - toUnwrap; _mint(msg.sender, FHE.decrypt(toUnwrap)); }
+    function _mintEncrypted(address to, inEuint128 memory encryptedAmount) internal { euint128 amount = FHE.asEuint128(encryptedAmount); _encBalances[to] = _encBalances[to] + amount; totalEncryptedSupply = totalEncryptedSupply + amount; }
+    function transferEncrypted(address to, inEuint128 calldata encryptedAmount) public returns (euint128) { return _transferImpl(msg.sender, to, FHE.asEuint128(encryptedAmount)); }
+    function _transferImpl(address from, address to, euint128 amount) internal returns (euint128) { euint128 send = FHE.select(amount.lte(_encBalances[from]), amount, FHE.asEuint128(0)); _encBalances[to] = _encBalances[to] + send; _encBalances[from] = _encBalances[from] - send; emit TransferEncrypted(from, to); return send; }
+    function balanceOfEncrypted(address account, Permission memory auth) virtual public view onlyPermitted(auth, account) returns (string memory) { return _encBalances[account].seal(auth.publicKey); }
 }
--- contracts/experimental/token/FHERC20/IFHERC20.sol
+++ contracts/experimental/token/FHERC20/IFHERC20.sol
@@ -1,102 +1,13 @@
 pragma solidity >=0.8.19 <0.9.0;
-
 // SPDX-License-Identifier: MIT
-// Fhenix Protocol (last updated v0.1.0) (token/FHERC20/IFHERC20.sol)
-// Inspired by OpenZeppelin (https://github.com/OpenZeppelin/openzeppelin-contracts) (token/ERC20/IERC20.sol)
-
 import { Permission, Permissioned } from "../../../access/Permissioned.sol";
 import { euint128, inEuint128 } from "../../../FHE.sol";
-
-/**
- * @dev Interface of the ERC-20 standard as defined in the ERC.
- */
 interface IFHERC20 {
-    /**
-     * @dev Emitted when `value` tokens are moved from one account (`from`) to
-     * another (`to`).
-     *
-     * Note that `value` may be zero.
-     */
     event TransferEncrypted(address indexed from, address indexed to);
-
-    /**
-     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
-     * a call to {approveEncrypted}. `value` is the new allowance.
-     */
     event ApprovalEncrypted(address indexed owner, address indexed spender);
-
-    // /**
-    //  * @dev Returns the value of tokens in existence.
-    //  */
-    // function totalSupply() external view returns (uint256);
-
-    /**
-     * @dev Returns the value of tokens owned by `account`, sealed and encrypted for the caller.
-     */
     function balanceOfEncrypted(address account, Permission memory auth) external view returns (string memory);
-
-    /**
-     * @dev Moves a `value` amount of tokens from the caller's account to `to`.
-     * Accepts the value as inEuint128, more convenient for calls from EOAs.
-     *
-     * Returns a boolean value indicating whether the operation succeeded.
-     */
-    function transferEncrypted(address to, inEuint128 calldata value) external returns (euint128);
-
-    /**
-     * @dev Moves a `value` amount of tokens from the caller's account to `to`.
-     * Accepts the value as euint128, more convenient for calls from other contracts
-     *
-     * Returns a boolean value indicating whether the operation succeeded.
-     */
-    function _transferEncrypted(address to, euint128 value) external returns (euint128);
-
-    /**
-     * @dev Returns the remaining number of tokens that `spender` will be
-     * allowed to spend on behalf of `owner` through {transferFrom}. This is
-     * zero by default.
-     *
-     * This value changes when {approve} or {transferFrom} are called.
-     */
     function allowanceEncrypted(address owner, address spender, Permission memory permission) external view returns (string memory);
-
-    /**
-     * @dev Sets a `value` amount of tokens as the allowance of `spender` over the
-     * caller's tokens.
-     *
-     * Returns a boolean value indicating whether the operation succeeded.
-     *
-     * IMPORTANT: Beware that changing an allowance with this method brings the risk
-     * that someone may use both the old and the new allowance by unfortunate
-     * transaction ordering. One possible solution to mitigate this race
-     * condition is to first reduce the spender's allowance to 0 and set the
-     * desired value afterwards:
-     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-     *
-     * Emits an {ApprovalEncrypted} event.
-     */
+    function transferEncrypted(address to, inEuint128 calldata value) external returns (euint128);
     function approveEncrypted(address spender, inEuint128 calldata value) external returns (bool);
-
-    /**
-     * @dev Moves a `value` amount of tokens from `from` to `to` using the
-     * allowance mechanism. `value` is then deducted from the caller's
-     * allowance. Accepts the value as inEuint128, more convenient for calls from EOAs.
-     *
-     * Returns a boolean value indicating whether the operation succeeded.
-     *
-     * Emits a {TransferEncrypted} event.
-     */
     function transferFromEncrypted(address from, address to, inEuint128 calldata value) external returns (euint128);
-
-    /**
-     * @dev Moves a `value` amount of tokens from `from` to `to` using the
-     * allowance mechanism. `value` is then deducted from the caller's
-     * allowance. Accepts the value as inEuint128, more convenient for calls
-     * from other contracts.
-     *
-     * Returns a boolean value indicating whether the operation succeeded.
-     *
-     * Emits a {TransferEncrypted} event.
-     */
-    function _transferFromEncrypted(address from, address to, euint128 value) external returns (euint128);
 }
